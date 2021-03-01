ac31f996727a93dd74d1333e5e80bb2a19212d51
Bonfra04 <bonfract@gmail.com>
2021-03-01 15:53:08 +0100

    called clib initializer

--- kernel/src/kernel.asm
+++ kernel/src/kernel.asm
@@ -2,10 +2,17 @@ bits 64
 
 section .start
     global _start
+    extern initialize_standard_library
     extern main
 
+bootinfo dq 0
+
 _start:
-    pop rdi     ; bootinfo parameter
+    pop qword [bootinfo]    ; preserve bootinfo parameter
+
+    call initialize_standard_library
+
+    mov rdi, [bootinfo]
     call main
 
 .hang:
