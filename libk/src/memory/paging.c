fbbf85e19213491839265c7422cebc650b33cdcb
Bonfra04 <bonfract@gmail.com>
2021-05-21 17:34:17 +0200

    -

--- libk/src/memory/paging.c
+++ libk/src/memory/paging.c
@@ -16,83 +16,13 @@
 
 #define PML_CLEAR_AVAILABLE(entry) (entry &= ~PML_AVAILABLE)
 #define PML_SET_AVAILABLE(entry, val) (entry |= ((val << 9) & PML_AVAILABLE))
-#define PML_UPDATE_AVAILABLE(entry, val) (PML_CLEAR_AVAILABLE(entry), PML_SET_AVAILABLE(entry, val));
+#define PML_UPDATE_AVAILABLE(entry, val) (PML_CLEAR_AVAILABLE(entry), PML_SET_AVAILABLE(entry, val))
 
 #define PML_CLEAR_ADDRESS(entry) (entry &= ~PML_ADDRESS)
 #define PML_SET_ADDRESS(entry, val) (entry |= ((val << 12) & PML_ADDRESS))
-#define PML_UPDATE_ADDRESS(entry, val) (PML_CLEAR_ADDRESS(entry), PML_SET_ADDRESS(entry, val));
+#define PML_UPDATE_ADDRESS(entry, val) (PML_CLEAR_ADDRESS(entry), PML_SET_ADDRESS(entry, val))
 
 #define PT_UPDATE_ADDRESS(entry, val) PML_UPDATE_ADDRESS(entry, val)
-
-/*
-typedef struct pml4_entry
-{
-   uint8_t present : 1;
-   uint8_t readwrite : 1;
-   uint8_t user : 1;
-   uint8_t writethrough : 1;
-   uint8_t cachedisable : 1;
-   uint8_t accessed : 1;
-   uint8_t reserved0 : 1;
-   uint8_t size : 1;
-   uint8_t reserved1 : 1;
-   uint8_t available : 3;
-   uint64_t address : 40;
-   uint16_t reserved2 : 11;
-   uint8_t execdisable : 1;
-} __attribute__((packed)) pml4_entry_t;
-
-typedef struct pdp_entry
-{
-   uint8_t present : 1;
-   uint8_t readwrite : 1;
-   uint8_t user : 1;
-   uint8_t writethrough : 1;
-   uint8_t cachedisable : 1;
-   uint8_t accessed : 1;
-   uint8_t reserved0 : 1;
-   uint8_t size : 1;
-   uint8_t reserved1 : 1;
-   uint8_t available : 3;
-   uint64_t address : 40;
-   uint16_t reserved2 : 11;
-   uint8_t execdisable : 1;
-} __attribute__((packed)) pdp_entry_t;
-
-typedef struct pd_entry
-{
-   uint8_t present : 1;
-   uint8_t readwrite : 1;
-   uint8_t user : 1;
-   uint8_t writethrough : 1;
-   uint8_t cachedisable : 1;
-   uint8_t accessed : 1;
-   uint8_t reserved0 : 1;
-   uint8_t size : 1;
-   uint8_t reserved1 : 1;
-   uint8_t available : 3;
-   uint64_t address : 40;
-   uint16_t reserved2 : 11;
-   uint8_t execdisable : 1;
-} __attribute__((packed)) pd_entry_t;
-*/
-
-typedef struct pt_entry
-{
-   uint8_t present : 1;
-   uint8_t readwrite : 1;
-   uint8_t user : 1;
-   uint8_t writethrough : 1;
-   uint8_t cachedisable : 1;
-   uint8_t accessed : 1;
-   uint8_t dirty : 1;
-   uint8_t reserved0 : 1;
-   uint64_t global : 1;
-   uint8_t available : 3;
-   uint64_t address : 40;
-   uint16_t reserved1 : 11;
-   uint8_t execdisable : 1;
-} __attribute__((packed)) pt_entry_t;
 
 paging_data_t paging_create()
 {
@@ -179,9 +109,7 @@ void paging_attach_2mb_page(paging_data_t data, void* physical_addr, void* virtu
         uint64_t* pdp = pfa_alloc_page();
         memset(pdp, 0, pfa_page_size());
 
-        pml4[pml4_offset] |= PML_PRESENT;
-        pml4[pml4_offset] |= PML_READWRITE;
-        pml4[pml4_offset] &= ~PML_USER; // set if using usermode
+        pml4[pml4_offset] = (PML_PRESENT | PML_READWRITE) & ~PML_USER;
         PML_UPDATE_ADDRESS(pml4[pml4_offset], (uint64_t)pdp);
     }
 
@@ -191,16 +119,11 @@ void paging_attach_2mb_page(paging_data_t data, void* physical_addr, void* virtu
         uint64_t* pd = pfa_alloc_page();
         memset(pd, 0, pfa_page_size());
 
-        pdp[pdp_offset] |= PML_PRESENT;
-        pdp[pdp_offset] |= PML_READWRITE;
-        pdp[pdp_offset] &= ~PML_USER; // set if using usermode
+        pdp[pdp_offset] = (PML_PRESENT | PML_READWRITE) & ~PML_USER;
         PML_UPDATE_ADDRESS(pdp[pdp_offset], (uint64_t)pd);
     }
 
     uint64_t* pd = (uint64_t*)((pdp[pdp_offset] & PML_ADDRESS) >> 12);
-    pd[pd_offset] |= PML_PRESENT;
-    pd[pd_offset] |= PML_READWRITE;
-    pd[pd_offset] &= ~PML_USER; // set if using usermode
-    pd[pd_offset] |= PML_SIZE; // 2mb page
+    pd[pd_offset] = (PML_PRESENT | PML_READWRITE | PML_SIZE) & ~PML_USER;
     PML_UPDATE_ADDRESS(pd[pd_offset], (uint64_t)physical_addr);
 }
