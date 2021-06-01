9082ee2402e9797c06e6c600a74cf11b33cdba76
Bonfra04 <bonfract@gmail.com>
2021-06-01 15:17:12 +0200

    qemu log

--- scripts/run_qemu.cmd
+++ scripts/run_qemu.cmd
@@ -2,4 +2,6 @@
 bash -c "make"
 echo Running
 
-start qemu-system-x86_64 -M q35 -m 512M -hda ./BonsOS.img -no-reboot -no-shutdown -S -gdb tcp::9000 -monitor stdio
+start qemu-system-x86_64 -M q35 -m 512M -drive file=./BonsOS.img,index=0,media=disk,format=raw -no-reboot -no-shutdown -S -gdb tcp::9000 -monitor stdio -d int -D qemu.log
+
+timeout 3
