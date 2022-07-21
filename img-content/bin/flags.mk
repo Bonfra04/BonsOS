f1017a37c30926071dd37eef41abb2fba0096730
Bonfra04 <bonfract@gmail.com>
2022-07-21 15:16:50 +0200

    no support for red zone in interrupt rn

--- img-content/bin/flags.mk
+++ img-content/bin/flags.mk
@@ -2,6 +2,7 @@ CCFLAGS	:= -std=gnu11 $(DIRS_INCLUDE) -g \
 			-D BONSOS \
 			-masm=intel \
 			-mcmodel=large \
+			-mno-red-zone \ # TODO: remove this (https://wiki.osdev.org/Calling_Conventions##:~:text=Note%202)
 			-Wall -Wextra \
 			-fplan9-extensions \
 			-Wno-misleading-indentation -Wno-parentheses -Wno-implicit-fallthrough \
