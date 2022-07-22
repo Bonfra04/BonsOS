b3626e7d610951d9271c84ecc71d1ceae359d9c2
Bonfra04 <bonfract@gmail.com>
2022-07-22 10:51:48 +0200

    some upsy

--- img-content/bin/flags.mk
+++ img-content/bin/flags.mk
@@ -2,8 +2,9 @@ CCFLAGS	:= -std=gnu11 $(DIRS_INCLUDE) -g \
 			-D BONSOS \
 			-masm=intel \
 			-mcmodel=large \
-			-mno-red-zone \ # TODO: remove this (https://wiki.osdev.org/Calling_Conventions##:~:text=Note%202)
+			-mno-red-zone \
 			-Wall -Wextra \
 			-fplan9-extensions \
 			-Wno-misleading-indentation -Wno-parentheses -Wno-implicit-fallthrough \
 			-Wno-sign-compare -Wno-address-of-packed-member -Wno-int-in-bool-context
+# TODO: remove -mno-red-zone (https://wiki.osdev.org/Calling_Conventions##:~:text=Note%202)
