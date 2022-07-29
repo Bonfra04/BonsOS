fdfcf44fcfed5713412d1678ff2fd7c010ed41ae
Bonfra04 <bonfract@gmail.com>
2022-07-29 12:24:03 +0200

    simple mutex implementation

---
+++ libbons/include/atomic/mutex.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdatomic.h>
+
+typedef atomic_uchar mutex_t;
+
+/**
+ * @brief waits for a mutex to be released, then locks it and return 
+ */
+void mutex_acquire(mutex_t* mutex);
+
+/**
+ * @brief releases a mutex
+ */
+void mutex_release(mutex_t* mutex);
---
+++ libbons/src/atomic/mutex.asm
@@ -0,0 +1,21 @@
+bits 64
+
+section .text
+    global mutex_acquire
+    global mutex_release
+    extern scheduler_yield
+
+mutex_acquire:
+.try_acquire:
+    xor rax, rax
+    lock bts [rdi], rax
+    jnc .done
+    call scheduler_yield ; replace block
+    jmp .try_acquire
+.done:
+    ret
+
+mutex_release:
+    xor rax, rax
+    lock btr [rdi], rax
+    ret
