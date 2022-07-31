471484a88285d3b354d3327fc8c4c3787d15e36c
Bonfra04 <bonfract@gmail.com>
2022-08-01 00:01:01 +0200

    calling ctors/dtors in reverse, how standard says

--- libc/src/init/ctors.c
+++ libc/src/init/ctors.c
@@ -5,7 +5,7 @@ typedef void (*ctor_t)();
 void call_ctors()
 {
     extern symbol_t __start_ctors, __end_ctors;
-    for(ctor_t* ctor = (ctor_t*)&__start_ctors; ctor < (ctor_t*)&__end_ctors; ctor++)
+    for(ctor_t* ctor = (ctor_t*)&__end_ctors - 1; ctor >= (ctor_t*)&__start_ctors; ctor--)
         if(*ctor)
             (*ctor)();
 }
--- libc/src/init/dtors.c
+++ libc/src/init/dtors.c
@@ -5,7 +5,7 @@ typedef void (*dtor_t)();
 void call_dtors()
 {
     extern symbol_t __start_dtors, __end_dtors;
-    for(dtor_t* dtor = (dtor_t*)&__start_dtors; dtor < (dtor_t*)&__end_dtors; dtor++)
+    for(dtor_t* dtor = (dtor_t*)&__end_dtors - 1; dtor >= (dtor_t*)&__start_dtors; dtor--)
         if(*dtor)
             (*dtor)();
 }
