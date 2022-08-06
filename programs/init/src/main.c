71ba7b2de497c20a5eeaf99aef16dfe49bc58fde
Bonfra04 <bonfract@gmail.com>
2022-08-06 22:42:25 +0200

    example shell

--- programs/init/src/main.c
+++ programs/init/src/main.c
@@ -5,18 +5,10 @@
 
 int main()
 {
+    // stream inherithed from childs
     fopen("tty:/0", "r"); // open stdin
     fopen("tty:/0", "w"); // open stdout
     fopen("tty:/0", "w"); // open stderr
-
-    puts("What's your name? ");
-    
-    char name[10];
-    gets(name); // buffer overflow goes brrrr
-    
-    fputs("Hello ", stdout);
-    fputs(name, stdout);
-    puts(".");
 
     sys_exec("a:/bin/shell.elf", NULL);
 
--- programs/shell/src/main.c
+++ programs/shell/src/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    puts("Hello from shell");
+    while(1)
+    {
+        char command[20];
+        fputs("$> ", stdout);
+        gets(command);
+
+        if(strncmp(command, "echo ", 5) == 0)
+            puts(command + 5);
+        else if(strncmp(command, "exit", 4) == 0)
+            break;
+        else
+            puts("Unknown command");
+    }
 
     return 0;
 }
