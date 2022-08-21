356d342a20b7e4cf7978cdf3cc213c030964fa73
Bonfra04 <bonfract@gmail.com>
2022-08-21 14:27:37 +0200

    allow operations to be single statement

--- libbons/include/containers/darray.h
+++ libbons/include/containers/darray.h
@@ -6,10 +6,10 @@ void* __darray(size_t elem_size, size_t length);
 #define darray(type, length) ((type*)__darray(sizeof(type), length))
 
 void* __darray_append(void* darray, void* element);
-#define darray_append(darray, element) { typeof(element) __tmp = element; darray = __darray_append(darray, &__tmp); }
+#define darray_append(darray, element) ({ typeof(element) __tmp = element; darray = __darray_append(darray, &__tmp); })
 
 void* __darray_remove(void* darray, size_t index);
-#define darray_remove(darray, index) { darray = __darray_remove(darray, index); }
+#define darray_remove(darray, index) ({ darray = __darray_remove(darray, index); })
 
 size_t __darray_find(void* darray, void* element);
 #define darray_find(darray, element) ({ typeof(element) __tmp = element; __darray_find(darray, &__tmp); })
