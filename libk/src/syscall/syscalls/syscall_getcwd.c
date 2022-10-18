915a83529b2fe8ba9749d088453c756c88cdbefe
Bonfra04 <bonfract@gmail.com>
2022-10-18 11:01:41 +0200

    bugfix

--- libk/src/syscall/syscalls/syscall_getcwd.c
+++ libk/src/syscall/syscalls/syscall_getcwd.c
@@ -6,12 +6,12 @@
 
 char* syscall_getcwd(char* buff, size_t size)
 {
-    buff =  paging_get_ph(current_thread->proc->paging, buff);
+    char* buff_addr = paging_get_ph(current_thread->proc->paging, buff);
 
     const char* wd = current_thread->proc->workdir;
     if(strlen(wd) >= size)
         return NULL;
 
-    strcpy(buff, wd);
+    strcpy(buff_addr, wd);
     return buff;
 }
