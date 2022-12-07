2838d23aa9132fb79b190598194cfd89fbe20113
Bonfra04 <bonfract@gmail.com>
2022-12-07 11:02:39 +0100

    better run qemu script

--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -1,9 +1,15 @@
+qemu_command="qemu-system-x86_64 -smp 1 -M q35 -m 512M
+-drive file=./BonsOS.img,index=0,media=disk,format=raw
+-nic none
+-drive if=none,id=stick,format=raw,file=./stick.img
+"
+
 if [ -z ${1+x} ]
 then
-    qemu-system-x86_64 -smp 1 -M q35 -m 512M -drive file=./BonsOS.img,index=0,media=disk,format=raw
+    $qemu_command
 elif [ $1 == "DEBUG" ]
 then
-    qemu-system-x86_64 -smp 1 -M q35 -m 512M -drive file=./BonsOS.img,index=0,media=disk,format=raw -no-reboot -no-shutdown -S -gdb tcp::9000 -d int -D qemu.log
+    $qemu_command -no-reboot -no-shutdown -S -gdb tcp::9000 -d int -D qemu.log
 else
     echo "Error"
 fi
